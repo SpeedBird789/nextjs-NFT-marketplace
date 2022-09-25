729331bb057a0205626a669af7f64a013b3e2e1c
Asif Mahmoud <asifmahmoud787@gmail.com>
2022-09-25 10:18:26 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
+import Header from "../components/Header";
+import Head from "next/head";
+
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL} initializeOnMount={false}>
+                <Header></Header>
+                <Component {...pageProps} />;
+            </MoralisProvider>
+        </div>
+    );
 }
 
-export default MyApp
+export default MyApp;
